b74ec6669343ddb797302e18eb9ea30b7fd5d013
EbenGonzalez <ebencito88@gmail.com>
2025-01-28 12:59:15 +0000

    DataInfo Actualizada + Minor Fixes

--- src/data/dataInfo.js
+++ src/data/dataInfo.js
@@ -1,11 +1,11 @@
 const buttons1 = [
   {
-    text: 'Nepe',
-    iconButton: '/icons/css_old.svg',
+    text: 'Repositorio',
+    iconButton: '/icons/Github_light.svg',
     link: 'https://www.linkedin.com/in/ebendev/',
   },
   {
-    text: 'Comprar Ahora',
+    text: 'Pruebame',
     iconButton: '/icons/device-gamepad.svg',
     link: 'https://ebengonzalez.github.io/Super_Memory_Bros/',
   },
@@ -15,16 +15,16 @@ const cards = [
   {
     id: 'card-1',
     images: ['/images/1.png', '/images/2.png', '/images/3.png'],
-    title: 'Proyecto 1',
-    description: 'Descripción del Proyecto 1 con detalles interesantes.',
+    title: 'Super Memory Bros',
+    description: 'Este fue mi primer proyecto en programación: un juego de memoria sencillo hecho con HTML, CSS y JavaScript. Por mi poca experiencia y conociemiento esta diseñado  pensando unicamente en pantallas de PC, por lo que no es responsive. Fue una experiencia muy útil para empezar a entender cómo funcionan las tecnologías básicas del desarrollo web',
     icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg'],
     buttons: buttons1
   },
   {
     id: 'card-2',
     images: ['/images/4.png', '/images/5.png', '/images/6.png'],
-    title: 'Proyecto 2',
-    description: 'Descripción del Proyecto 2, un enfoque diferente.',
+    title: 'Insulink',
+    description: 'Una pequeña API que, a partir de los informes generados por una bomba de insulina, proporciona información sobre las cantidades de hidratos de carbono e insulina consumidos, resistencia a la insulina y otras utilidades pensadas tanto para diabéticos como para sus cuidadores.',
     icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/Express.js_light.svg', '/icons/mysql.svg',
       '/icons/postman.svg', '/icons/nodejs.svg', '/icons/git.svg', '/icons/sequelize.svg'],
     buttons: []
@@ -32,11 +32,20 @@ const cards = [
   {
     id: 'card-3',
     images: ['/images/7.png', '/images/8.png'],
-    title: 'Proyecto 3',
-    description: 'Descripción del Proyecto 3 con un resumen atractivo.',
-    icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg'],
+    title: 'Way Home',
+    description: 'Proyecto final del bootcamp, donde aplicamos todo lo aprendido para crear una web interactiva destinada a amantes de los animales. La plataforma facilita la interacción entre usuarios que buscan dar en adopción o ofrecer acogida temporal a sus mascotas.',
+    icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/Express.js_light.svg', '/icons/mysql.svg',
+      '/icons/postman.svg', '/icons/nodejs.svg', '/icons/git.svg', '/icons/sequelize.svg', '/icons/react.svg'],
     buttons: []
   },
+  {
+    id: 'card-4',
+    images: ['/images/7.png', '/images/8.png'],
+    title: 'Portfolio Personal',
+    description: 'Mis primeros pasos con Astro y Tailwind para crear este pequeño portfolio personal. Está organizado por componentes y es fácil de usar como plantilla para quien lo necesite.',
+    icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/nodejs.svg', '/icons/tailwindcss.svg', '/icons/Astro_light.svg','/icons/flowbite.svg'],
+    buttons: []
+  }
 ];
 
 const intro1= "Hola soy ebencito";
