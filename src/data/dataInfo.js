6edf583b69862e952e529c4371e210d09812c218
EbenGonzalez <ebencito88@gmail.com>
2025-01-28 18:53:46 +0000

    Header Fixed + Minor Fixes

--- src/data/dataInfo.js
+++ src/data/dataInfo.js
@@ -2,13 +2,42 @@ const buttons1 = [
   {
     text: 'Repositorio',
     iconButton: '/icons/Github_light.svg',
-    link: 'https://www.linkedin.com/in/ebendev/',
+    link: 'https://github.com/EbenGonzalez/Super_Memory_Bros',
   },
   {
     text: 'Pruebame',
     iconButton: '/icons/device-gamepad.svg',
     link: 'https://ebengonzalez.github.io/Super_Memory_Bros/',
   },
+];
+
+const buttons2 = [
+  {
+    text: 'Repositorio',
+    iconButton: '/icons/Github_light.svg',
+    link: 'https://github.com/EbenGonzalez/Project-2-Insulink',
+  }
+];
+
+const buttons3 = [
+  {
+    text: 'Backend',
+    iconButton: '/icons/Github_light.svg',
+    link: 'https://github.com/EbenGonzalez/Project-WayHome-Backend',
+  },
+  {
+    text: 'Frontend',
+    iconButton: '/icons/Github_light.svg',
+    link: 'https://github.com/EbenGonzalez/Project-WayHome-Frontend',
+  },
+];
+
+const buttons4 = [
+  {
+    text: 'Repositorio',
+    iconButton: '/icons/Github_light.svg',
+    link: 'https://github.com/EbenGonzalez/Astro_Portfolio',
+  }
 ];
 
 const cards = [
@@ -27,7 +56,7 @@ const cards = [
     description: 'Una pequeña API que, a partir de los informes generados por una bomba de insulina, proporciona información sobre las cantidades de hidratos de carbono e insulina consumidos, resistencia a la insulina y otras utilidades pensadas tanto para diabéticos como para sus cuidadores.',
     icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/Express.js_light.svg', '/icons/mysql.svg',
       '/icons/postman.svg', '/icons/nodejs.svg', '/icons/git.svg', '/icons/sequelize.svg'],
-    buttons: []
+    buttons: buttons2
   },
   {
     id: 'card-3',
@@ -36,7 +65,7 @@ const cards = [
     description: 'Proyecto final del bootcamp, donde aplicamos todo lo aprendido para crear una web interactiva destinada a amantes de los animales. La plataforma facilita la interacción entre usuarios que buscan dar en adopción o ofrecer acogida temporal a sus mascotas.',
     icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/Express.js_light.svg', '/icons/mysql.svg',
       '/icons/postman.svg', '/icons/nodejs.svg', '/icons/git.svg', '/icons/sequelize.svg', '/icons/react.svg'],
-    buttons: []
+    buttons: buttons3
   },
   {
     id: 'card-4',
@@ -44,7 +73,7 @@ const cards = [
     title: 'Portfolio Personal',
     description: 'Mis primeros pasos con Astro y Tailwind para crear este pequeño portfolio personal. Está organizado por componentes y es fácil de usar como plantilla para quien lo necesite.',
     icons: ['/icons/html5.svg', '/icons/css_old.svg', '/icons/javascript.svg', '/icons/vscode.svg', '/icons/nodejs.svg', '/icons/tailwindcss.svg', '/icons/Astro_light.svg','/icons/flowbite.svg'],
-    buttons: []
+    buttons: buttons4
   }
 ];
 
